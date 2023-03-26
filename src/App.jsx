c251ea479022c513d30045c5c06cb71d781410f4
negnq <117580177+neginashkan@users.noreply.github.com>
2023-03-26 16:22:24 +0430

    Initial setup of folders and files for React project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import "./css/styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;

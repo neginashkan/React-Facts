bb695036439366eb83ad2188a31c55577b679423
negnq <117580177+neginashkan@users.noreply.github.com>
2023-03-27 07:42:47 +0430

    Finished Header Section of Website
    This commit includes the completion of the header section of the website, including the logo and toggle button. This section is now complete and ready for review.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,43 @@
+import { useState } from "react";
+import react_icon from "../images/react-icon.png";
+
 function Header() {
-  return (<header className="Header"></header>)
+  const [isToggled, setIsToggled] = useState(false);
+  function handleToggle(event) {
+    const { checked } = event.target;
+    checked ? setIsToggled(true) : setIsToggled(false);
+  }
+  return (
+    <header className="Header">
+      <div className="icon--title">
+        <img className="icon" src={react_icon} alt="React Icon" title="React" />
+        <h2 className="title">ReactFacts</h2>
+      </div>
+      <div className=" theme--toggle--button ">
+        <form>
+          <input
+            onChange={handleToggle}
+            id="switch-button"
+            name="switch-button"
+            type="checkbox"
+            className="input-checkbox"
+            value={isToggled}
+          />
+          <label htmlFor="switch-button" className="toggle">
+            toggle
+          </label>
+          <span  className="light--text label-text">
+            light
+          </span>
+          <span className="dark--text label-text">
+            dark
+          </span>
+        </form>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
+
+
